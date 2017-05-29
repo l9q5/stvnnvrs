525412c4bdcd8c8618fe94b12e21d2078cf2a919
l9q5 <l9q5@hotmail.com>
2017-05-29 16:25:23 -0300

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html onkeydown="myFunction(event)">
 	<head>
 	<title>Query string </title>
@@ -49,7 +48,7 @@
 
 	<video id="Video1" class="center" controls="" autoplay="" name="media">
 	<source src="https://a.safe.moe/EGjYq.mp4" type="video/mp4">
-	<track label="English" kind="subtitles" srclang="en" src="s5e1.vtt" default>
+	<track label="English" kind="subtitles" srclang="en" src="https://raw.githubusercontent.com/l9q5/stvnnvrs/main/captions/vtt/s5e1.vtt" default>
 	<a href="demo.mp4">Download the video</a> file.
 	</video>
 
@@ -61,72 +60,3 @@
 
 	</body>
 	</html>
-	
-=======
-<html onkeydown="myFunction(event)">
-	<head>
-	<title>Query string </title>
-	<meta name="viewport" content="width=device-width, user-scalable=no">
-	<link rel="stylesheet" href="a.css">
-
-	<script type="text/javascript">
-
-    function vidplay() {
-       var video = document.getElementById("Video1");
-       var button = document.getElementById("play");
-       if (video.paused) {
-          video.play();
-          button.textContent = "||";
-       } else {
-          video.pause();
-          button.textContent = ">";
-       }
-    }
-    function myFunction(event) {
-    
-    var x = event.which || event.keyCode;
-    	if (x == 37) {
-    	var video = document.getElementById("Video1");
-        video.currentTime += -2;
-    	}
-    	if (x == 39) {
-    	var video = document.getElementById("Video1");
-        video.currentTime += 2;
-    	}
-    	if (x == 32) {
-        vidplay();
-    	}
-	}
-    function restart() {
-        var video = document.getElementById("Video1");
-        video.currentTime = 0;
-    }
-
-    function skip(value) {
-        var video = document.getElementById("Video1");
-        video.currentTime += value;
-    }      
-	</script>
-
-	</head>
-	<body style="margin: 0px;display: block;">
-
-	<?php
-
-	
-	?>
-	<video id="Video1" class="center" controls="" autoplay="" name="media">
-	<source src="https://a.safe.moe/EGjYq.mp4" type="video/mp4">
-	<track label="Português" kind="subtitles" srclang="pt" src="captions/vtt/s5e1.vtt" default>
-	</video>
-
-
-  
-
-
-
-
-	</body>
-	</html>
-	
->>>>>>> 22657be41866bee7ac59b2f0dad33607c2172cd0
